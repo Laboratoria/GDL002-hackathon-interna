37bdebcb61a7f024147f32e6c75096b9e80991ea
Melisa Quesada <melisa.quc@gmail.com>
2019-03-15 12:25:15 -0600

    Juntamos indelas páginas Index, Story y Suits, Añadimos función Modal

---
+++ story.js
@@ -0,0 +1,3 @@
+document.getElementById("storyPage").addEventListener("click", ()=> window.location.href = "story.html");
+document.getElementById("moviesPage").addEventListener("click", ()=> window.location.href = "index.html");
+document.getElementById("suitesPage").addEventListener("click", ()=> window.location.href = "suits.html");
